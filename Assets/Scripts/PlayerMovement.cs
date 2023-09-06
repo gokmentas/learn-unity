b9589e593941d13c5796f6025786f4d07a8697ce
gokmentas <56593104+gokmentas@users.noreply.github.com>
2023-09-06 14:36:39 +0300

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed = 5f;
+    public float speed = 10f;
 
     // Start is called before the first frame update
     void Start()
