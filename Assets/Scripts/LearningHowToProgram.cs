fe1123d8555b41296e700115c2cdf10bb12353fa
gokmentas <56593104+gokmentas@users.noreply.github.com>
2023-09-07 14:01:05 +0300

    Update LearningHowToProgram.cs

--- Assets/Scripts/LearningHowToProgram.cs
+++ Assets/Scripts/LearningHowToProgram.cs
@@ -7,18 +7,39 @@ public class LearningHowToProgram : MonoBehaviour
     private void Start()
     {
 
+        //CalculateTwoNumbers();
+        //CalculateUnknownNumbers(30, 22);
+
+        //float sum = returnTwoNumbers();
+        //Debug.Log("The sum is: " + sum);
+
+        Debug.Log($"The sum is: {CalculateSum(10, 20)}");
+
+    }
+
+    // a function that doesn't return a value and doesn't take parameters
+    void CalculateTwoNumbers()
+    {
         float a = 20;
         float b = 7;
-        float c = a * b;
+        float c = a + b;
+        Debug.Log($"The sum of {a} and {b} is: {c}");
+    }
 
-        string name = "carl";
-        string lastName = " florian";
+    // a function that doesn't return a value but takes parameters
+    void CalculateUnknownNumbers(float a, float b)
+    {
+        Debug.Log($"The sum of {a} and {b} is: {a + b}");
+    }
 
-        string fullName = name + lastName;
+    float returnTwoNumbers()
+    {
+        return 20 + 30;
+    }
 
-        //Debug.Log(a + " / " + b + " = " + c);
-        Debug.Log(fullName);
-
+    float CalculateSum(float a, float b)
+    {
+        return (a + b);
     }
 
 } // class
