ae55f45a138c0e4dcf57fd95d69fd2eaac83cd5f
gokmentas <56593104+gokmentas@users.noreply.github.com>
2023-09-07 23:08:46 +0300

    Update LearningHowToProgram.cs

--- Assets/Scripts/LearningHowToProgram.cs
+++ Assets/Scripts/LearningHowToProgram.cs
@@ -6,39 +6,17 @@ public class LearningHowToProgram : MonoBehaviour
 {
     private void Start()
     {
-        float health = 101;
+        StartCoroutine(ExecuteSomething(2));
 
-        switch (health)
-        {
-            case 100:
-                Debug.Log("Health is 100");
-                break;
-
-            case 50:
-                Debug.Log("Health is 50");
-                break;
-
-            case 0:
-                Debug.Log("Health is 0");
-                break;
-
-            default:
-                Debug.Log("Healt is neighter of the value above");
-                break;
-        }
-
-        //if (health == 0)
-        //{
-        //    // if health is less than zero, then condition is true
-        //    // code that is here, will be executed
-        //    Debug.Log("Health is 0 player has died");
-        //}else if (health < 50)
-        //{
-        //    Debug.Log("Health is less than 50 unlock some power for player");
-        //}
-        //else
-        //{
-        //    Debug.Log("Player has health greater than 50");
-        //}
+        StartCoroutine("ExecuteSomething");
+        StopCoroutine("ExecuteSomething");
     }
+
+    IEnumerator ExecuteSomething(float time)
+    {
+        yield return new WaitForSeconds(time);
+
+        Debug.Log("Something is executed");
+    }
+
 } // class
